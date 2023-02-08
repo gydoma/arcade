166593b6a5bd2e8326f2f28f6d37df4401162dbc
gydoma <65687471+gydoma@users.noreply.github.com>
2023-02-08 08:29:20 +0100

    web 0.1.3a

---
+++ index/scss/components/custom/christmas.php
@@ -0,0 +1,51 @@
+<!--Print it between nav and body - christmas lights -->
+<?php
+$xmasStart = new DateTime('11/1 00:00:00');
+$xmasEnd = new DateTime('1/5 23:59:59');
+$now = new DateTime();
+if($now >= $xmasStart && $now < $xmasEnd) {
+    echo '<ul class="lightrope">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>';
+}
