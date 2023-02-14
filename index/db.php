767531fe45bd7db9c4b2ad30490650510e322142
KatRoland <katona.roland@verebelyszki.hu>
2023-02-15 00:04:49 +0100

    debux text remove

--- index/db.php
+++ index/db.php
@@ -18,7 +18,5 @@ foreach ($lines as $line) {
     $con = mysqli_connect($host,$user,$password,$database);
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    } else{
-        echo "fasza";
-    } 
+    }
 ?>
