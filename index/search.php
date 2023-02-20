781fc9ea18b871fcf17655c9b62ce0a1592e6d0d
gydoma <65687471+gydoma@users.noreply.github.com>
2023-02-20 18:56:13 +0100

    0.3.1.1a
    few changes, indenting, commenting, titles

--- index/search.php
+++ index/search.php
@@ -1,10 +1,9 @@
-
-    <div class="roundedcorners flex device-m" id="filter">
-        <p>Search & filter</p>
-        <img src="Resources/feather/search.svg" id="filterimg">
-        <img src="Resources/cat.svg" id="cat">
-    </div>
-    <div class="device-s" id="filterbtn">
-        <img src="Resources/button/search.svg">
-    </div>
-    <script src="javascript/searchbutton.js"></script>
+<div class="roundedcorners flex device-m" id="filter">
+    <p>Search & filter</p>
+    <img src="Resources/feather/search.svg" id="filterimg">
+    <img src="Resources/cat.svg" id="cat">
+</div>
+<div class="device-s" id="filterbtn">
+    <img src="Resources/button/search.svg">
+</div>
+<script src="javascript/searchbutton.js"></script>
