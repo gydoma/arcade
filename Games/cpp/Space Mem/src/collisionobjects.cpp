7515d2429f130b5f0c986fa5d36b8e56c2d3f514
keirahela <mirkamarkate@gmail.com>
2023-02-28 19:19:46 +0100

    first commit

---
+++ Games/cpp/Space Mem/src/collisionobjects.cpp
@@ -0,0 +1,8 @@
+#include "collisionobjects.h"
+
+// Objectek "felkészítése"
+
+class Bullet;
+class Enemy;
+std::list<Bullet *> bulletCollisionObjects;
+std::list<Enemy *> enemyCollisionObjects;
---
+++ Games/cpp/Space Mem/src/main.cpp
@@ -0,0 +1,48 @@
+#include "globals.h"
+#include "raylib.h"
+#include "gameobject.h"
+#include "background.h"
+#include "player.h"
+#include "collisionhandler.h"
+#include "enemyspawner.h"
+#include "musicplayer.h"
+#include "scoredisplay.h"
+#include <list>
+#include <iostream>
+
+int main(void)
+{
+    InitWindow(1920, 1080, "Space Mem");
+
+    Background bg;
+    CollisionHandler handler;
+    Player player;
+    EnemySpawner spawner;
+    MusicPlayer musicPlayer;
+    ScoreDisplay display;
+
+    // Fő game loop
+    while (!WindowShouldClose())
+    {
+        // Update
+        for (GameObject* gameObject : gameObjects)
+        {
+            gameObject->Update();
+        }
+
+        // "Rajzolás"
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        for (GameObject *gameObject : gameObjects)
+        {
+            gameObject->Render();
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
