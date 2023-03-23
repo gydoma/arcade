82b0b96eb931b1e1ac5420f8bc253248ac7f8bf6
gydoma <65687471+gydoma@users.noreply.github.com>
2023-03-23 14:01:50 +0100

    web 0.4.3a
    ### Changes:
    
    index.php - added .body-content div
    fixed body height
    footer load teleporting
    few other fixes
    
    ### +:
    download.php
    config.scss
    - .big-card
     _button_download.scss
    - btn-download/:hover, container
    
    ### Known Bugs Came With The Commit:
    .btn-download - strange visual behavior onclick

--- index/download.php
+++ index/download.php
@@ -10,19 +10,19 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="body-content">
     <?php include 'navbar.php'?>
     <?php include 'php/christmas.php'?>
-    <br>
-    <div class="games"></div>
-    
 
-    <?php include 'filter.php'; ?>
-    <?php include 'search.php'; ?>
+    <div class="card shadow big-card">
+        <div class="download-container">
+            <h1>Game Title</h1>
+            <p>Game Description</p>
+
+            <a class="btn-download" href="index/" download="example.txt">Download</a>
+        </div>
+    </div>
 
     <?php include 'footer.php'; ?>
-    <div class="push"></div>
-    </div>
     
     <script src="javascript/games.js"></script>
     <script src="javascript/darkmode.js" onload="darkmodecheck()"></script>
