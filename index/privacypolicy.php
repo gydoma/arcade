d5ce6a446043596c92e469dd840878f76010f2e6
gydoma <65687471+gydoma@users.noreply.github.com>
2023-05-11 14:56:15 +0200

    web 0.6.2a
    - Added Privacy Policy

---
+++ index/privacypolicy.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chromium Arcade - Adatvédelmi Irányelvek</title>
+    <link rel="stylesheet" href="/index/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="body-content">
+        <?php include 'navbar.php'?>
+        <?php include 'php/christmas.php'?>
+        <div class="roundedcorners card">
+            <h1>Adatvédelmi irányelvek</h1>
+            <p>A Chromium Network tiszteletben tartja a felhasználók magánéletét, és elkötelezett a felhasználók által velünk megosztott személyes adatok védelme mellett. Ez az adatvédelmi szabályzat elmagyarázza, hogy a Chromium Network hogyan gyűjti, használja és osztja meg a személyes adatokat.</p>
+            <h2>Milyen személyes adatokat gyűjt a Chromium Network?</h2>
+            <p>A Chromium Network többféle módon gyűjt személyes adatokat a felhasználóktól. Amikor a felhasználók fiókot hoznak létre a Chromium Networkön, gyűjtjük a felhasználónevüket, e-mail címüket és jelszavukat. A felhasználók IP-címét és böngészőinformációit is gyűjtjük, amikor meglátogatják a weboldalunkat.</p>
+            <h2>Hogyan használja fel a Chromium Network a személyes adatokat?</h2>
+            <p>A Chromium Network a személyes adatokat arra használja fel, hogy a felhasználók számára az általuk kért szolgáltatásokat nyújtsa. Például a felhasználók felhasználónevét és e-mail címét arra használjuk, hogy fiókinformációkat és értesítéseket küldjünk nekik. A felhasználók IP-címét és a böngészőre vonatkozó információkat is felhasználjuk, hogy segítsünk a weboldalunk és szolgáltatásaink javításában.</p>
+            <h2>Megosztja a Chromium Network a személyes adatokat harmadik felekkel?</h2>
+            <p>A Chromium Network nem osztja meg személyes adatait harmadik felekkel marketingcélokra. Megoszthatunk azonban személyes adatokat olyan harmadik felekkel, akik szolgáltatásokat nyújtanak számunkra, például a weboldalunk tárhelyét. Ezek a harmadik felek szerződéses kötelezettséget vállalnak arra, hogy a felhasználók személyes adatait bizalmasan és biztonságosan kezelik.</p>
+            <h2>Hogyan férhetnek hozzá, javíthatják vagy törölhetik személyes adataikat a felhasználók?</h2>
+            <p>A felhasználók személyes adataikhoz hozzáférhetnek, kijavíthatják vagy törölhetik azokat, ha bejelentkeznek fiókjukba, és a "Profil" oldalra lépnek. A felhasználók Gyurkovics Dominikhoz is fordulhatnak a gydoma@chromiumnetwork.com e-mail címen, hogy hozzáférést kérjenek személyes adataikhoz, kijavítsák vagy töröljék azokat.</p>
+            <h2>Hogyan védi a Chromium Network a személyes adatokat?</h2>
+            <p>A Chromium Network lépéseket tesz annak érdekében, hogy megvédje a felhasználók személyes adatait a jogosulatlan hozzáféréstől, felhasználástól, közzétételtől, megváltoztatástól vagy megsemmisítéstől. Ezek a lépések a következők:</p>
+            <ul>
+                <li>Az iparági szabványoknak megfelelő biztonsági intézkedések alkalmazása rendszereink és adataink védelme érdekében.</li>
+                <li>Minden alkalmazott és vállalkozó titoktartási megállapodás aláírása.</li>
+                <li>Rendszeres biztonsági ellenőrzések elvégzése</li>
+                <li>a biztonsági incidensekre való gyors és hatékony reagálás</li>
+                <li>A jelen adatvédelmi szabályzat módosításai</li>
+            </ul>
+            <p>A Chromium Network időről időre frissítheti ezt az adatvédelmi szabályzatot. Ha jelentős változásokat eszközlünk a személyes adatok gyűjtésének, felhasználásának vagy megosztásának módjában, erről a weboldalunkon jól látható módon értesítjük a felhasználókat.</p>
+            <h2>Kapcsolatfelvétel</h2>
+            <p>Ha bármilyen kérdése van ezzel az adatvédelmi politikával kapcsolatban, kérjük, forduljon Gyurkovics Dominikhoz a gydoma@chromiumnetwork.com e-mail címen.</p>
+            <button onclick="document.location='index.php'" id="agreebutton">Értettem, vissza a főoldalra</button>
+        </div>
+
+<?php include 'footer.php'; ?>
+<div class="push"></div>
+</div>
+<script src="javascript/games.js"></script>
+<script src="javascript/darkmode.js" onload="darkmodecheck()"></script>
+</body>
+</html>
